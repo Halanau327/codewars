30bb36b55c53899f04acfe6f997d2d10fefbc1d9
Halanau327 <a.halanau@gmail.com>
2024-02-09 21:43:07 +0300

    add codewars tasks

--- codewars/7kuy/descending_order.js
+++ codewars/7kuy/descending_order.js
@@ -1,5 +1,7 @@
 function descendingOrder(numb) {
-    let numbersArray = String(numb).split("").map(Number);
+    let numbersArray = String(numb).split("");
     numbersArray.sort((a, b) => b - a);
-    return numbersArray.join("");
-}   
+    return Number(numbersArray.join(""));
+}
+
+descendingOrder(215432562);

65e235e8f734b0a169d2d0df58566c7bb853ff94
Halanau327 <a.halanau@gmail.com>
2024-04-08 23:10:12 +0300

    training js native

---
+++ codewars/7kuy/withoutLast.js
@@ -0,0 +1,5 @@
+function withoutLast(arr) {
+    // Fix it
+    arr.pop(); // removes the last element
+    return arr;
+}
---
+++ codewars/nativeJS/training.js
@@ -0,0 +1,97 @@
+// // let fruits = ['apple', 'banana', 'orange'];
+// // fruits.push('cherry')
+// // console.log(fruits)
+//
+// const users = [
+//     {
+//         id: 1,
+//         name: 'Bob',
+//         isStudent: true,
+//     },
+//     {
+//         id: 2,
+//         name: 'Alex',
+//         isStudent: true,
+//     },
+//     {
+//         id: 3,
+//         name: 'Ann',
+//         isStudent: true,
+//     },
+//     {
+//         id: 4,
+//         name: 'Donald',
+//         isStudent: false,
+//     },
+// ];
+//
+// // const poppedElement = users.pop()
+// // console.log(poppedElement)
+//
+// // const shiftedElement = users.shift()
+// // console.log(shiftedElement)
+// // console.log(users)
+//
+// const el = {
+//     id: 0,
+//     name: 'TestName',
+//     isStudent: false
+// }
+// const unshiftedElement = users.unshift(el)
+// console.log(unshiftedElement)
+// console.log(users)
+
+// // const str = 'JavaScript is awesome';
+// // console.log(str.split(' ').reverse().join(' '))
+//
+// let fruits = ['apple', 'banana', 'orange'],["1", "2"];
+//
+// let games = ['cs 2', 'dota 2', 'wow'];
+//
+// // const mergeArrays = fruits.concat(games)
+// console.log(mergeArrays)
+//
+// let flattenArray = [1,
+//     [2, 3],
+//     [[4], [5, 6]
+//     ]
+// ]
+//
+// let flatArr = flattenArray.flat(Infinity)
+// console.log(flatArr)
+//
+// let flattenArray2 = [1,
+//     [2,
+//         [3,
+//             [4,
+//                 [5]
+//             ]
+//         ]
+//     ]
+// ]
+//
+// let flatArr2 = flattenArray2.flat(Infinity)
+// console.log(flatArr2)
+
+// let arr = [1, 2, 3, 4, 5];
+
+// const calculateSquare = (array) => {
+//     array.forEach((num) => {
+//         let squaredNum = num ** 2;
+//         console.log(`${num} squared is ${squaredNum}`)
+//     })
+// }
+//
+// calculateSquare(arr)
+
+// let arr = [10, 20, 30, 40, 50]
+//
+// let elOfArr = arr.find(e => e > 25)
+// console.log(elOfArr)
+
+// let fruits = ["яблоко", "банан", "апельсин", "киви"]
+//
+// let foundFruit = fruits.find(el => el.startsWith("а"))
+//
+// console.log(foundFruit)
+

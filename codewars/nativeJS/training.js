138b3fb32e17b97d4919d4906427086b3f09bb02
Halanau <a.halanau@gmail.com>
2024-04-09 15:26:04 +0300

    added solutions for tasks lesson 2 native JS

--- codewars/nativeJS/training.js
+++ codewars/nativeJS/training.js
@@ -69,7 +69,7 @@
 //         ]
 //     ]
 // ]
-//
+
 // let flatArr2 = flattenArray2.flat(Infinity)
 // console.log(flatArr2)
 
@@ -81,7 +81,7 @@
 //         console.log(`${num} squared is ${squaredNum}`)
 //     })
 // }
-//
+
 // calculateSquare(arr)
 
 // let arr = [10, 20, 30, 40, 50]
@@ -89,9 +89,86 @@
 // let elOfArr = arr.find(e => e > 25)
 // console.log(elOfArr)
 
-// let fruits = ["яблоко", "банан", "апельсин", "киви"]
-//
-// let foundFruit = fruits.find(el => el.startsWith("а"))
-//
-// console.log(foundFruit)
+// // let fruits = ["яблоко", "банан", "апельсин", "киви"]
+// //
+// // let foundFruit = fruits.find(el => el.startsWith("а"))
+// //
+// // console.log(foundFruit)
 
+// // let numbers = [10, 20, 30, 40, 50]
+// const findFirstElement = (array, condition) => {
+//     return array.find(condition)
+// }
+// // let firstNumberGreaterThan25 = findFirstElement(numbers, (num) => num > 25);
+
+// // console.log(firstNumberGreaterThan25)
+
+// let fruits = ["яблоко", "банан", "апельсин", "киви"]
+
+// let fruitStartsWithO = findFirstElement(fruits, (fruit) => fruit.startsWith("а"));
+// console.log(fruitStartsWithO)
+// let array =  [1, 2, 3, 4, 5]
+// let fruits =  ["яблоко", "банан", "апельсин", "киви"]
+// const checkElement = (arr, el) => {
+//     return arr.includes(el);
+// }
+
+// console.log(checkElement(fruits,"яблоко"))
+
+// let numbers = [10, 20, 30, 40, 50];
+// let numbers2 = [5, 15, 25, 35, 45]
+//
+// const filterGreaterThan = (array) => {
+//     return array.filter(arr => arr > 20)
+// }
+// console.log(filterGreaterThan(numbers2))
+//
+// let people = [
+//     { name: 'John', age: 30 },
+//     { name: 'Alice', age: 25 },
+//     { name: 'Bob', age: 35 },
+// ];
+
+// const sortedPeople = (arr) => {
+//     return arr.sort((a,b) => b-a)
+// }
+//
+// console.log(sortedPeople(people))
+
+//
+// let numbers = [5, 2, 8, 1, 4];
+//
+// const sortedPeople = (arr) => {
+//     return arr.sort((a,b) => b-a)
+// }
+//
+// console.log(sortedPeople(numbers))
+// let data = [
+//     {value: 1, name: 'Option 1'},
+//     {value: 2, name: 'Option 2'},
+//     {value: 3, name: 'Option 3'},
+// ];
+//
+// let formattedData = data.map((d) => {
+//     return {
+//         value: d.value,
+//         label: d.name
+//     }
+// })
+// console.log(formattedData)
+
+
+let products = [
+    {id: 1, name: 'Apple', price: 1.99},
+    {id: 2, name: 'Banana', price: 0.99},
+    {id: 3, name: 'Orange', price: 2.49},
+];
+
+const productsWithDiscount = products.map((discount) => {
+    return {
+        ...products,
+        dicsountedPrice: discount.price * 0.9
+    }
+})
+
+console.log(productsWithDiscount)

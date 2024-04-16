865ce7d593f966de7ecad729e7733978d27ca7f3
Halanau <a.halanau@gmail.com>
2024-04-16 09:00:50 +0300

    practise native JS

--- codewars/8kuy/divisible-by-given-number.js
+++ codewars/8kuy/divisible-by-given-number.js
@@ -2,9 +2,14 @@ arr = [1, 2, 3, 4, 5, 6]
 
 function divisibleBy(numbers, divisor) {
     let newArray = numbers.map((d) => {
-        return d / divisor === Number.isInteger ? d : NaN
+        debugger
+        return Number.isInteger(d / divisor) ? d : NaN;
     })
     return newArray.filter(Number.isInteger)
 }
 
 divisibleBy(arr, 2)
+
+// function divisibleBy(numbers, divisor) {
+//     return numbers.filter(n => n % divisor === 0)
+// }

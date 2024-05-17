d5b2de9a187e4a0645530c34f74b905412851b61
Halanau <a.halanau@gmail.com>
2024-05-17 16:13:35 +0300

    training codewars

--- codewars/7kuy/find-the-middle-element.js
+++ codewars/7kuy/find-the-middle-element.js
@@ -1,18 +1,9 @@
 let arr = [5,6,1]
 
 function gimme(triplet) {
-    let one = triplet[0]
-    let two = triplet[1]
-    let three = triplet[2]
-    return triplet.map((t, index) => {
-        if (two < one < three || two > one > three) {
-            return one[index]
-        } else if (one < two < three || one > two > three) {
-            return two[index]
-        } else if (one < three < two || one > three > two) {
-            return three[index]
-        }
-    })
+    let newValue = [...triplet].sort((a,b) => a-b)
+    let middle = newValue[1]
+    return triplet.indexOf(middle)
 }
 
 console.log(gimme(arr))
